3d1fd05c7d8d7fde1e3c08958c84046ed7317d40
donghyo8 <donghyo@dongguk.edu>
2020-02-09 21:25:29 +0900

    lecutre 생성 로직 작성

--- sql/index.js
+++ sql/index.js
@@ -1,7 +1,17 @@
 module.exports = {
 	selectUserByUsernameAndPassword : "select * from users where username = ? and password = ?",
-	insertUser: "insert into users (username, password, studentNumber, name) values ( ?, ?, ?, ? )",
 	selectProjects: "select * from projects",
 	selectProjectById: "select * from projects where id = ?",
-	insertProject: "insert into projects (user_id, name, discription, path) values( ?, ?, ?, ? )"
-};
+	selectLectures: "select * from lectures",
+	selectLecturesById : "select * from lectures where id = ?",
+	selectLecturesByUserId : "select * from lectures where id in (select lecture_id from users_lectures where user_id = ? and end) ",
+	selectLecturesByYear : "select * from lectures where season_year = ? and season_quarter = ?",
+	selectLectures_seasonYear : "select * from lectures where season_year = ?",
+	insertLectures_admin : "insert into lectures (title, description, season_year, season_quarter, lecture_type, major, class_number,class_name, score, professor) values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
+	selectLectures_admin : "select * from users where username = ?",
+	
+ 
+	/* Insert task */
+	insertProject: "insert into projects (user_id, name, discription, path) values( ?, ?, ?, ? )",
+	insertUser: "insert into users (username, password, studentNumber, name) values ( ?, ?, ?, ? )"
+ };
