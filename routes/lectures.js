3ec81d7f809c10a1e39ec01e481f8f8b07de05c2
lynever <lyenver@naver.com>
2020-02-09 21:19:22 +0900

    users put api

--- routes/lectures.js
+++ routes/lectures.js
@@ -1,11 +1,22 @@
 var express = require('express');
 var router = express.Router();
+var db = require('../modules/db-connection-pool');
+const sql = require('../sql');
+
 
 /**
  * 전체 lecture들을 조회한다.
  */
-router.get("/", function(req, res) {
-
+router.get("/", async function(req, res) {
+	res.status(200)
+	try{
+		var [rows] = await db.query(sql.selectLectures, []);
+		
+	}
+	catch(err){
+		console.log("강의를 조회할 수 없습니다.")
+    }
+    res.send(rows);
 });
 
 
@@ -13,8 +24,8 @@ router.get("/", function(req, res) {
  * 특정 lecture들을 조회한다.
  * 이때 존재하지 않으면 404 error
  */
-router.get("/:lecture_id", function(req, res) {
-
+router.get("/:lecture_id", async function(req, res) {
+ 
 });
 
 /**
--- sql/index.js
+++ sql/index.js
@@ -3,5 +3,7 @@ module.exports = {
 	insertUser: "insert into users (username, password, studentNumber, name) values ( ?, ?, ?, ? )",
 	selectProjects: "select * from projects",
 	selectProjectById: "select * from projects where id = ?",
-	insertProject: "insert into projects (user_id, name, discription, path) values( ?, ?, ?, ? )"
+	insertProject: "insert into projects (user_id, name, discription, path) values( ?, ?, ?, ? )",
+	selectLectures: "select * from lectures",
+	updateEnable: "update users_lectures set enabled = 0 where user_id = ? and lecture_id = ?"
 };
