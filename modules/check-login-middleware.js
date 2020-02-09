7b941f9df490b1b409f0479460882cf08ede7ead
donghyo8 <donghyo@dongguk.edu>
2020-02-09 21:25:05 +0900

    admin 추가

--- modules/check-login-middleware.js
+++ modules/check-login-middleware.js
@@ -5,7 +5,7 @@ async function injectUser(req, res, next) {
 	req.user = {
 		id: 1, username: "2016112187", name: "김현수",
 		email: "manist67@naver.com", major: "컴퓨터공학과",
-		created: new Date(), updated: new Date()
+		created: new Date(), updated: new Date(), admin : true
 	};
 	next();
 	/*const conn = await pool.getConnection(async conn => conn);
