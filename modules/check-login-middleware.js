7a19c0814c8e6eddbc2e6d4173a23a30568a6655
김현수 <manist67@naver.com>
2020-02-28 05:33:01 +0900

    PREVENT NOOOOO

--- modules/check-login-middleware.js
+++ modules/check-login-middleware.js
@@ -2,11 +2,7 @@
 var pool = require('../modules/db-connection-pool');
 
 async function injectUser(req, res, next) {
-	req.user = {
-		id: 1, username: "2016112187", name: "김현수",
-		email: "manist67@naver.com", major: "컴퓨터공학과",
-		created: new Date(), updated: new Date(), admin : true
-	};
+	req.user = { id: Number(req.headers.id), isAdmin: true };
 	next();
 	/*const conn = await pool.getConnection(async conn => conn);
 	const [rows] = await conn.query();
